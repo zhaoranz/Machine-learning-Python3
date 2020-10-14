df400efa6eea8acc4341445d28e26e50ee04df74
zhaoranz <zhaoran1124@gmail.com>
2020-10-14 17:28:30 +0200

    Update Pytorch-introduction.py

--- Pytorch-introduction.py
+++ Pytorch-introduction.py
@@ -2,7 +2,7 @@
 import torch
 import torchvision
 from torchvision import transforms, datasets
-
+#download mnist dataset:
 train=datasets.MNIST('', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
 
 test=datasets.MNIST('', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
@@ -12,7 +12,9 @@ test=datasets.MNIST('', train=False, download=True, transform=transforms.Compose
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
 
-#batch size is for the large-scle data
+#batch size is 1. for the large sample size, cannot fit any realistic example on GPU   2. generalization avoiding arbitrary optimazation
+#14
+
 
 
 
