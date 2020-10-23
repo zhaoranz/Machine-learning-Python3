255d54ba41b60261f263ff42b7e4c3bf479a0de3
zhaoranz <zhaoran1124@gmail.com>
2020-10-23 13:56:47 +0200

    Update Pytorch-introduction.py

--- Pytorch-introduction.py
+++ Pytorch-introduction.py
@@ -17,6 +17,36 @@ testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Net(nn.Module):
+  def __init__(self):
+    super().__init__()#this is important
+    self.fc1=nn.Linear(784, 64)
+    self.fc2=nn.Linear(64, 64)
+    self.fc3=nn.Linear(64, 64)
+    self.fc4=nn.Linear(64, 10)
+  def forward(self, x):
+    x=F.relu(self.fc1(x))#why not set different activation functions for different neurons?
+    x=F.relu(self.fc2(x))
+    x=F.relu(self.fc2(x))
+    x=self.fc4(x) 
+    
+    return F.log_softmax(x,dim=1)#dim-0, batches
+    
+net=Net()
+print(net)
+
+#some data:
+x=torch.rand((28*28))
+X=x.view(-1,28*28)
+output=net(X)
+
+
+
+    
+    
+    
+    
+
 
 
 
