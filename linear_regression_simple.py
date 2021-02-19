058b5f8e0f618589e9ffae3a2c25d1019266d343
zhaoranz <zhaoran1124@gmail.com>
2021-02-19 14:23:36 +0100

    Update linear_regression_simple.py

--- linear_regression_simple.py
+++ linear_regression_simple.py
@@ -1,1 +1,11 @@
 # How to perform linear regression using sklearn.
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+!wget -O FuelConsumption.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv
+  
+df = pd.read_csv("file.csv")
+df.describe()
